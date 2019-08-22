cd355ecceb7551b50d98cf2786e73edfbd2b6e71
Bea Maia <beatrizmatiasmaia@gmail.com>
2019-08-22 13:03:48 -0300

    Exercicio 1

--- aula-1/ex1.c
+++ aula-1/ex1.c
@@ -1,147 +1,234 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define TAM 40
-
-//Imprima a lista de aniversariante ordenado por matricula
-
-typedef struct{
-    float p1;
-    float p2;
-    float t1;
-    float t2;
-    float t3;
-    float media;
-} tNotas;
+#define MAX_ALUNOS 100
+#define MAX_NOME 50
 
 typedef struct{
     int dia;
     int mes;
     int ano;
-} tAniversario;
-
-typedef struct{
-    int dia;
-    int mes;
-    int ano;
-} tIngresso;
+} tData;
 
 typedef struct{
     int matricula;
-    char nome[50];
-    tNotas notas;
-    tAniversario niver;
-    tIngresso ingresso;
+    char nome[MAX_NOME];
+    float cr;
+    tData niver;
+    tData ingresso;
     int situacao;
 } tAluno;
 
-int leia_matricula(tAluno*);
-float calcula_media(tAluno);
-int situacao_final(float);
-void ordena_matriculas(tAluno *, int);
-void printa_alunos(tAluno *, int);
+//Funcoes para leitura
+tData leia_data();
+int leia_matricula(tAluno []);
+
+//Funcao para modo
+void selecao(tAluno[], int, tData);
+int apresenta_pergunta();
+void seleciona_modo(tAluno [], int, int, tData);
+
+void imprime_informacoes(tAluno);
+
+//Funcoes ordenacao
+void ordena_nome(tAluno [], int);
+void ordena_matricula(tAluno [], int);
+void ordena_ingresso(tAluno [], int);
+void ordena_cr(tAluno [], int);
+
+//Funcoes de apresentacao
+void apresenta_informacoes(tAluno [], int);
+void apresenta_aniversariantes(tAluno [], tData, int);
+void apresenta_excelentes(tAluno [], int);
+
 
 int main()
 {
-    tAluno aluno[TAM];
+    tData hoje;
+    hoje = leia_data();
+
+    tAluno aluno[MAX_ALUNOS];
     int qtd;
 
     qtd = leia_matricula(aluno);
-    ordena_matriculas(aluno, qtd);
-    printa_alunos(aluno, qtd);
+    selecao(aluno, qtd, hoje);
 
     return 0;
 }
 
-int leia_matricula(tAluno *aluno)
+void selecao(tAluno aluno[], int qtd_aluno, tData hoje)
 {
-    int num;
-    for(num = 0; ; num++)
+    int entrada;
+
+    entrada = apresenta_pergunta();
+    seleciona_modo(aluno, qtd_aluno, entrada, hoje);
+
+}
+
+int apresenta_pergunta()
+{
+    int entrada; 
+
+    printf("\nEscolha um mode de apresentação:\n");
+    printf("1: Todos alunos por ordem de matricula\n");
+    printf("2: Todos alunos por ordem alfabetica\n");
+    printf("3: Informação dos aniversariantes\n");
+    printf("4: Informação dos excelentes\n");
+    scanf("%d", &entrada);
+
+    return entrada;
+}
+
+void seleciona_modo(tAluno aluno[], int qtd_alunos, int entrada, tData hoje)
+{
+    switch(entrada)
+    {
+        case 1: ordena_matricula(aluno, qtd_alunos);
+                apresenta_informacoes(aluno, qtd_alunos);
+                break;
+        case 2: ordena_nome(aluno, qtd_alunos);
+                apresenta_informacoes(aluno, qtd_alunos);
+                break;
+        case 3: ordena_matricula(aluno, qtd_alunos);
+                apresenta_aniversariantes(aluno, hoje, qtd_alunos);
+                break;
+        case 4: ordena_matricula(aluno, qtd_alunos);
+                apresenta_excelentes(aluno, qtd_alunos);
+                break;
+        default: break;
+    }
+}
+
+tData leia_data()
+{
+    tData hoje;
+
+    scanf("%d/%d/%d", &hoje.dia, &hoje.mes, &hoje.ano);
+    return hoje;
+}
+
+int leia_matricula(tAluno aluno[])
+{
+    int num = 0;
+
+    printf("Matricula: ");
+    scanf("%d%*c", &aluno[num].matricula);
+
+    for(; num < MAX_ALUNOS && aluno[num].matricula != 0; num++)
     {
         printf("Nome: ");
         scanf("%[^\n]", aluno[num].nome);
 
-        printf("Matricula: ");
-        scanf("%d", &aluno[num].matricula);
-
-        if(aluno[num].matricula == 0)
-            break;
-
-        printf("Nota P1: ");
-        scanf("%f", &aluno[num].notas.p1);
-
-        printf("Nota P2: ");
-        scanf("%f", &aluno[num].notas.p2);
-
-        printf("Nota T1: ");
-        scanf("%f", &aluno[num].notas.t1);
-
-        printf("Nota T2: ");
-        scanf("%f", &aluno[num].notas.t2);
-
-        printf("Nota T3: ");
-        scanf("%f", &aluno[num].notas.t3);
+        printf("CR: ");
+        scanf("%f%*c", &aluno[num].cr);
 
         printf("Aniversário: ");
-        scanf("%d/%d/%d", &aluno[num].niver.dia, &aluno[num].niver.mes, &aluno[num].niver.ano);
+        scanf("%d/%d/%d%*c", &aluno[num].niver.dia, &aluno[num].niver.mes, &aluno[num].niver.ano);
 
         printf("Ingresso: ");
-        scanf("%d/%d/%d%*c", &aluno[num].ingresso.dia, &aluno[num].ingresso.mes, &aluno[num].ingresso.ano);
-
-        aluno[num].notas.media = calcula_media(aluno[num]);
-        aluno[num].situacao = situacao_final(aluno[num].notas.media);        
+        scanf("%d/%d/%d%*c", &aluno[num].ingresso.dia, &aluno[num].ingresso.mes, &aluno[num].ingresso.ano); 
+        
+        printf("Matricula: ");
+        scanf("%d%*c", &aluno[num + 1].matricula);     
     }
 
     return num;
 }
 
-float calcula_media(tAluno aluno)
-{
-    float media;
-    media = ((aluno.notas.p1 * aluno.notas.t1) + (aluno.notas.p2 * aluno.notas.t2))/20 + aluno.notas.t3/10;
-    return media;
-}
-
-int situacao_final(float media)
-{
-    if(media >= 7)
-        return 1;
-    else
-        return 0;
-}
-
-void ordena_matriculas(tAluno * aluno, int qtd_alunos)
+void ordena_nome(tAluno aluno[], int qtd_alunos)
 {
     tAluno temp;
     char nome_temp[16];
 
     for(int i = 0; i < qtd_alunos - 1; i++)
     {
-        for(int j = i; j < qtd_alunos; j++)
+        for(int j = 0; j < qtd_alunos - 1; j++)
         {
-            if(strcmp(aluno[i].nome, aluno[j].nome) > 0) 
+            if(strcmp(aluno[i].nome, aluno[j].nome) < 0) 
             {
                 temp = aluno[j];
-                aluno[j] = aluno[i];
-                aluno[i] = temp;
+                aluno[j] = aluno[j + 1];
+                aluno[j + 1] = temp;
             }
         }
     }
 }
 
-void printa_alunos(tAluno *aluno, int qtd_alunos)
+void ordena_matricula(tAluno aluno[], int qtd_alunos)
 {
-    if(qtd_alunos == 0)
+    tAluno temp;
+
+    for(int i = 0; i < qtd_alunos - 1; i++)
+    {
+        for(int j = 0; j < qtd_alunos - 1; j++)
+        {
+            if(aluno[i].matricula > aluno[j].matricula) 
+            {
+                temp = aluno[j];
+                aluno[j] = aluno[j + 1];
+                aluno[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void ordena_ingresso(tAluno aluno[], int qtd_alunos)
+{
+}
+
+void ordena_cr(tAluno aluno[], int qtd_alunos)
+{
+    tAluno temp;
+
+    for(int i = 0; i < qtd_alunos - 1; i++)
+    {
+        for(int j = 0; j < qtd_alunos - 1; j++)
+        {
+            if(aluno[i].cr < aluno[j].cr) 
+            {
+                temp = aluno[j];
+                aluno[j] = aluno[j + 1];
+                aluno[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void imprime_informacoes(tAluno aluno)
+{
+    printf("Aluno: %s\n", aluno.nome);                                                                        
+    printf("Matricula: %d\n", aluno.matricula);
+    printf("CR: %.2f\n", aluno.cr);
+    printf("Aniversário: %02d/%02d/%02d\n\n", aluno.niver.dia, aluno.niver.mes, aluno.niver.ano);
+}
+
+void apresenta_informacoes(tAluno aluno[], int qtd_alunos)
+{
+    if(!qtd_alunos)
         exit(1);
 
     for(int i = 0; i < qtd_alunos; i++)
     {
-        printf("\nAluno: %s\nMedia Parcial: %.2f\nPrecisa de prova final?: ", aluno[i].nome, aluno[i].notas.media);
-
-        if(aluno[i].situacao == 1)
-            printf("Não \n");
-        else
-            printf("Sim \n");
+        imprime_informacoes(aluno[i]);
     }
 }
 
+void apresenta_aniversariantes(tAluno aluno[], tData hoje, int qtd_alunos)
+{
+    printf("Aniversariantes: \n\n");
+
+    for(int i = 0; i < qtd_alunos; i++)
+    {
+        if(hoje.dia == aluno[i].niver.dia && hoje.mes == aluno[i].niver.mes)
+            imprime_informacoes(aluno[i]);
+    }
+}
+
+void apresenta_excelentes(tAluno aluno[], int qtd_aluno)
+{   
+    for(int i = 0; i < qtd_aluno; i++)
+    {
+        if(aluno[i].cr >= 8)
+            imprime_informacoes(aluno[i]);
+    }
+}
