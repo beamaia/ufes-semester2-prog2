1247d5f9fd546b641955f342d6594f030c9c52ba
Bea Maia <beatrizmatiasmaia@gmail.com>
2019-09-19 14:49:48 -0300

    19/09

--- lista-1/l101/Structs_01.c
+++ lista-1/l101/Structs_01.c
@@ -10,42 +10,9 @@ typedef struct {
    int p, q, sinal;
 } racional;
 
-void leia_racional(racional *r)
-{
-   scanf("%d", &r->p);
-   scanf("%d", &r->q);
-}
-
-int tem_sinal(int num)
-{
-   return num < 0;
-}
-
 int divisivel(int num, int div)
 {
    return num % div == 0;
-}
-
-void numero_negativo(racional * r)
-{
-   if(tem_sinal(r->p) && tem_sinal(r->q))
-   {
-      r->sinal = 0;
-      r->p = abs(r->p);
-      r->q = abs(r->q);
-   }
-   else if(tem_sinal(r->q))
-   {
-      r->sinal = 1;
-      r->q = abs(r->q);
-   }
-   else if(tem_sinal(r->p))
-   {
-      r->sinal = 1;
-      r->p = abs(r->p);
-   }
-   else
-      r->sinal = 0;
 }
 
 int num_maior(int n1, int n2)
@@ -53,50 +20,34 @@ int num_maior(int n1, int n2)
    return n1 >= n2;
 }
 
-int limite_min(racional *r)
+int limite_min(int a, int b)
 {
-   if(num_maior(r->p, r->q))
-      return r->q;
+   if(num_maior(a, b))
+      return b;
    else
-      return r->p;
+      return a;
 }
 
-void reduz (racional *r, int lim) 
+racional reduz (int a, int b) 
 {
-   for(int i = lim; i >= 1; i--)
+   if(b < 0)
    {
-      if(divisivel(r->p, i) && divisivel(r->q, i))
-      {
-         r->p = r->p/i;
-         r->q = r->q/i;
-      }
-   }   
-}
-
-void calcula(racional * r)
-{
-   int lim;
-   numero_negativo(r);
-   lim = limite_min(r);
-   reduz(r, lim);
-}
-
-void apresenta(racional r)
-{
-   if(r.sinal)
-      printf("-");
-   
-   printf("%d/%d\n", r.p, r.q);
-}
-
-int main()
-{
-   racional numero;
-   leia_racional(&numero);
-   if(numero.q != 0)
-   {
-      calcula(&numero);
-      apresenta(numero);
+      b = b * -1;
+      a = a * -1;
    }
-   return 0;
+
+   int lim = limite_min(a, b);
+   for(int i = lim; i >= 2; i--)
+   {
+      if(divisivel(a, i) && divisivel(b, i))
+      {
+         a = a/i;
+         b = b/i;
+      }
+   }  
+
+   racional r;
+   r.p = a;
+   r.q = b;
+   return r;
 }
